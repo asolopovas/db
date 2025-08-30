b4fc939af3a45cb5dc8017bd7ec46f562e10bcf3
andrius <andrius.solopovas@gmail.com>
2025-08-30 16:11:27 +0100

    save

--- database/seeders/PopulateOrderPartialsSeeder.php
+++ database/seeders/PopulateOrderPartialsSeeder.php
@@ -15,7 +15,6 @@ class PopulateOrderPartialsSeeder extends Seeder
             PopulateOrderServiceNamesSeeder::class,
             PopulateOrderMaterialNamesSeeder::class,
             PopulateProductAreaNamesSeeder::class,
-            PopulateOrderIdsSeeder::class,
         ]);
     }
 }
