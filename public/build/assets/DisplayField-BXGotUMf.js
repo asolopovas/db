82c6e9ef87f93ee8419a896e2840f445e77de10b
andrius <andrius.solopovas@gmail.com>
2025-08-30 16:10:56 +0000

    release

--- public/build/assets/DisplayField-BXGotUMf.js
+++
@@ -1,1 +0,0 @@
-import{_ as m}from"./DisplayField.vue_vue_type_script_setup_true_lang-DTCtrHSr.js";import"./app-Cyu1s19O.js";export{m as default};
---
+++ public/build/assets/DisplayField-ClVIC7z3.js
@@ -0,0 +1,1 @@
+import{_ as m}from"./DisplayField.vue_vue_type_script_setup_true_lang-CewYbBrQ.js";import"./app-Cf3AaWPo.js";export{m as default};
