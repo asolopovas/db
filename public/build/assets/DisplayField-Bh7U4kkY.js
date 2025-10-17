ce55904ca2862bfbdfd9725d102ebf512545c409
andrius <andrius.solopovas@gmail.com>
2025-10-17 12:39:30 +0000

    updated titles

--- public/build/assets/DisplayField-Bh7U4kkY.js
+++
@@ -1,1 +0,0 @@
-import{_ as m}from"./DisplayField.vue_vue_type_script_setup_true_lang-CsTRzZgX.js";import"./app-CZIMIw-m.js";export{m as default};
---
+++ public/build/assets/DisplayField-DYpkGMbl.js
@@ -0,0 +1,1 @@
+import{_ as m}from"./DisplayField.vue_vue_type_script_setup_true_lang-C1oF4NtB.js";import"./app-BbdjgHJc.js";export{m as default};
